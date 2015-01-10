4c1d58a86d8c0832103863f86d8f5cedaf334959
Denis Yoshchyk <bycter@gmail.com>
2015-01-10 14:27:43 +0300

    Добавил 1. Переменные для подключения к базе 2. Обработчики OpenConnection() - заполнил CloseConnection() - заполнил Insert() Delete() Select() Count() Backup() Restore()
    3.
    label1 - view connectionString,
    label2 - status CloseConnection()

--- TemperatureGraphs_MySql/MainForm.Designer.cs
+++ TemperatureGraphs_MySql/MainForm.Designer.cs
@@ -28,20 +28,86 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.SuspendLayout();
-            // 
-            // MainForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(374, 341);
-            this.Name = "MainForm";
-            this.Text = "График температуры";
-            this.ResumeLayout(false);
+			this.btConnectDB = new System.Windows.Forms.Button();
+			this.btViewTable = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btConnectDB
+			// 
+			this.btConnectDB.Location = new System.Drawing.Point(12, 293);
+			this.btConnectDB.Name = "btConnectDB";
+			this.btConnectDB.Size = new System.Drawing.Size(113, 36);
+			this.btConnectDB.TabIndex = 0;
+			this.btConnectDB.Text = "Подключение к БД";
+			this.btConnectDB.UseVisualStyleBackColor = true;
+			this.btConnectDB.Click += new System.EventHandler(this.btConnectDB_Click);
+			// 
+			// btViewTable
+			// 
+			this.btViewTable.Location = new System.Drawing.Point(385, 293);
+			this.btViewTable.Name = "btViewTable";
+			this.btViewTable.Size = new System.Drawing.Size(113, 36);
+			this.btViewTable.TabIndex = 1;
+			this.btViewTable.Text = "Вывести таблицу";
+			this.btViewTable.UseVisualStyleBackColor = true;
+			this.btViewTable.Click += new System.EventHandler(this.btViewTable_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(486, 241);
+			this.dataGridView1.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 256);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(10, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = ".";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 277);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(10, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = ".";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(510, 341);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.btViewTable);
+			this.Controls.Add(this.btConnectDB);
+			this.Name = "MainForm";
+			this.Text = "График температуры";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button btConnectDB;
+		private System.Windows.Forms.Button btViewTable;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
 	}
 }
 
--- TemperatureGraphs_MySql/MainForm.cs
+++ TemperatureGraphs_MySql/MainForm.cs
@@ -6,15 +6,139 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 
 namespace TemperatureGraphs_MySql
 {
 	public partial class MainForm : Form
 	{
+		private MySqlConnection connection;
+		private string server;
+		private string database;
+		private string uid;
+		private string password;
+
 		public MainForm()
 		{
 			InitializeComponent();
 		}
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			string connectionString;
+			server = "10.8.0.10";
+			uid = "smarthouse";
+			password = "U4PD2cKIAs";
+			database = "smarthouse";
+
+			connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+			// connectionString = "SERVER=10.8.0.10;DATABASE=smarthouse;UID=smarthouse;PASSWORD=U4PD2cKIAs";
+			connection = new MySqlConnection(connectionString);
+
+			label1.Text = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + "********" + ";";
+		}
+
+		private void btConnectDB_Click(object sender, EventArgs e)
+		{
+			if (OpenConnection())
+			{
+				MessageBox.Show("Connection open.");
+			}
+			if (!CloseConnection())
+			{
+				label2.Text = "Connection NOT close.";
+			}
+			else
+			{
+				label2.Text = "Connection close.";
+			}
+		}
+
+		private void btViewTable_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		//open connection to database
+		private bool OpenConnection()
+		{
+			try
+			{
+				connection.Open();
+				return true;
+			}
+			catch (MySqlException ex)
+			{
+				//When handling errors, you can your application's response based 
+				//on the error number.
+				//The two most common error numbers when connecting are as follows:
+				//0: Cannot connect to server.
+				//1045: Invalid user name and/or password.
+				switch (ex.Number)
+				{
+					case 0:
+						MessageBox.Show("Cannot connect to server.  Contact administrator");
+						break;
+
+					case 1045:
+						MessageBox.Show("Invalid username/password, please try again");
+						break;
+				}
+				return false;
+			}
+		}
+
+		//Close connection
+		private bool CloseConnection()
+		{
+			try
+			{
+				connection.Close();
+				return true;
+			}
+			catch (MySqlException ex)
+			{
+				MessageBox.Show(ex.Message);
+				return false;
+			}
+		}
+
+		
+
+		////Insert statement
+		//public void Insert()
+		//{
+		//}
+
+		////Update statement
+		//public void Update()
+		//{
+		//}
+
+		////Delete statement
+		//public void Delete()
+		//{
+		//}
+
+		////Select statement
+		//public List<string>[] Select()
+		//{
+		//}
+
+		////Count statement
+		//public int Count()
+		//{
+		//}
+
+		////Backup
+		//public void Backup()
+		//{
+		//}
+
+		////Restore
+		//public void Restore()
+		//{
+		//}
 	}
 }
